8f609889b836e26ea67c3515a0cd75f37d3ad9a8
TendoCryzx <amfinezxc@hotmail.com>
2023-10-31 02:38:18 +0700

    ฟกกฟไำ

---
+++ lib/components/TimeStart.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TimeStartComponents extends StatefulWidget {
+  @override
+  _TimeStartComponentsState createState() => _TimeStartComponentsState();
+}
+
+class _TimeStartComponentsState extends State<TimeStartComponents> {
+  String? selectedHours; // To store the selected number of hours.
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.watch_later_rounded),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0, bottom: 8),
+              child: Text(
+                'เวลาเริ่มงาน :',
+                style: GoogleFonts.kanit(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        ),
+        DropdownButtonFormField<String>(
+          value: selectedHours,
+          items: <DropdownMenuItem<String>>[
+            DropdownMenuItem<String>(
+              value: '9.00 น.',
+              child: Text(
+                '9.00 น.',
+                style: GoogleFonts.kanit(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            DropdownMenuItem<String>(
+              value: '10.00 น.',
+              child: Text(
+                '10.00 น.',
+                style: GoogleFonts.kanit(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            DropdownMenuItem<String>(
+              value: '11.00 น.',
+              child: Text(
+                '11.00 น.',
+                style: GoogleFonts.kanit(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            DropdownMenuItem<String>(
+              value: '13.00 น.',
+              child: Text(
+                '13.00 น.',
+                style: GoogleFonts.kanit(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            DropdownMenuItem<String>(
+              value: '14.00 น.',
+              child: Text(
+                '14.00 น.',
+                style: GoogleFonts.kanit(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            DropdownMenuItem<String>(
+              value: '15.00 น.',
+              child: Text(
+                '15.00 น.',
+                style: GoogleFonts.kanit(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            DropdownMenuItem<String>(
+              value: '16.00 น.',
+              child: Text(
+                '16.00 น.',
+                style: GoogleFonts.kanit(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+          onChanged: (String? newValue) {
+            setState(() {
+              selectedHours = newValue;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
