2656763f23c81500cf9e3fb446923a82d7e11080
Teddymalaju123 <127287397+Teddymalaju123@users.noreply.github.com>
2023-10-31 23:08:26 +0700

    add

--- lib/screens/Pay_Page.dart
+++ lib/screens/Pay_Page.dart
@@ -12,77 +12,63 @@ class PayPage extends StatefulWidget {
 
 class _PayPageState extends State<PayPage> {
   File? _selectedImage;
+  String? image;
 
   Future<void> _selectImage() async {
     final ImagePicker _picker = ImagePicker();
-    XFile? _image;
+    XFile? image = await _picker.pickImage(source: ImageSource.gallery);
 
-    Future getImage() async {
-      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-
+    if (image != null) {
       setState(() {
-        _image = image;
+        _selectedImage = File(image!.path);
+        final bytes = _selectedImage!.readAsBytesSync();
+        image = base64Encode(bytes) as XFile?;
       });
     }
+  }
 
-    String? image;
+  void _showImageSourceActionSheet(BuildContext context) {
+    Function(ImageSource) selectImageSource = (imageSource) {
+      _selectImage();
+    };
 
-    Future<void> chooseFile(ImageSource source) async {
-      final ImagePicker picker = ImagePicker();
-      final XFile? pickedFile = await picker.pickImage(
-        source: source,
-        maxWidth: 500,
-        imageQuality: 100,
-      );
-
-      if (pickedFile == null) return;
-      final bytes = await pickedFile.readAsBytes();
-      final String base64String = base64Encode(bytes);
-      image = base64String;
-      setState(() {});
-    }
-
-    void _showImageSourceActionSheet(BuildContext context) {
-      Function(ImageSource) selectImageSource = (imageSource) {
-        chooseFile(imageSource);
-      };
-
-      if (Platform.isIOS) {
-        showCupertinoModalPopup(
-          context: context,
-          builder: (context) => CupertinoActionSheet(
-            actions: [
-              CupertinoActionSheetAction(
-                child: Text(
-                  'ถ่ายภาพ',
-                  style: TextStyle(
-                    fontFamily: 'anupark',
-                  ),
+    if (Platform.isIOS) {
+      showCupertinoModalPopup(
+        context: context,
+        builder: (context) => CupertinoActionSheet(
+          actions: [
+            CupertinoActionSheetAction(
+              child: Text(
+                'ถ่ายภาพ',
+                style: TextStyle(
+                  fontFamily: 'anupark',
                 ),
-                onPressed: () {
-                  Navigator.pop(context);
-                  selectImageSource(ImageSource.camera);
-                },
               ),
-              CupertinoActionSheetAction(
-                child: Text(
-                  'เลือกภาพถ่าย',
-                  style: TextStyle(
-                    fontFamily: 'anupark',
-                  ),
+              onPressed: () {
+                Navigator.pop(context);
+                selectImageSource(ImageSource.camera);
+              },
+            ),
+            CupertinoActionSheetAction(
+              child: Text(
+                'เลือกภาพถ่าย',
+                style: TextStyle(
+                  fontFamily: 'anupark',
                 ),
-                onPressed: () {
-                  Navigator.pop(context);
-                  selectImageSource(ImageSource.gallery);
-                },
-              )
-            ],
-          ),
-        );
-      } else {
-        showModalBottomSheet(
-          context: context,
-          builder: (context) => Wrap(children: [
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+                selectImageSource(ImageSource.gallery);
+              },
+            )
+          ],
+        ),
+      );
+    } else {
+      showModalBottomSheet(
+        context: context,
+        builder: (context) => Wrap(
+          children: [
             ListTile(
               leading: Icon(Icons.camera_alt),
               title: Text(
@@ -109,83 +95,89 @@ class _PayPageState extends State<PayPage> {
                 selectImageSource(ImageSource.gallery);
               },
             ),
-          ]),
-        );
-      }
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 232, 241, 230),
-          title: Text(
-            'ชำระเงิน',
-            style: GoogleFonts.kanit(
-              textStyle: TextStyle(color: Colors.green),
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          centerTitle: true,
-          iconTheme: IconThemeData(color: Colors.green),
-        ),
-        body: Container(
-          color: Color.fromARGB(255, 232, 241, 230),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                if (_selectedImage != null)
-                  Image.file(_selectedImage!)
-                else
-                  Text(
-                    'แนบหลักฐานการชำระเงิน',
-                    style: GoogleFonts.kanit(
-                      textStyle: TextStyle(color: Colors.black),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 20,
-                    ),
-                  ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ElevatedButton(
-                      onPressed: _selectImage,
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green,
-                      ),
-                      child: Text('เลือกรูปภาพ'),
-                    ),
-                  ],
-                ),
-                if (_selectedImage != null)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          // Handle the payment logic here
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.green,
-                        ),
-                        child: Text('ชำระเงิน'),
-                      ),
-                    ],
-                  ),
-              ],
-            ),
-          ),
+          ],
         ),
       );
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 232, 241, 230),
+        title: Text(
+          'ชำระเงิน',
+          style: GoogleFonts.kanit(
+            textStyle: TextStyle(color: Colors.green),
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.green),
+      ),
+      body: Container(
+        color: Color.fromARGB(255, 232, 241, 230),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (_selectedImage != null)
+                Image.file(_selectedImage!)
+              else
+                Text(
+                  'แนบหลักฐานการชำระเงิน',
+                  style: GoogleFonts.kanit(
+                    textStyle: TextStyle(color: Colors.black),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                  ),
+                ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _selectImage,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                    ),
+                    child: Text('เลือกรูปภาพ'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              if (image != null)
+                CircleAvatar(
+                  radius: 120,
+                  backgroundImage: MemoryImage(base64Decode("${image}")),
+                ),
+              SizedBox(height: 20.0),
+              Text(
+                "รูปสัตว์เลี้ยง",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 15.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 125, 152, 91),
+                  minimumSize: const Size.fromHeight(50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {
+                  _showImageSourceActionSheet(context);
+                },
+                child: Text('อัปโหลดรูปสัตว์เลี้ยง'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
