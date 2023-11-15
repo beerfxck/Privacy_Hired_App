b50db9fca935d7ade340028bfa78ac1d68e6c4c1
Teddymalaju123 <127287397+Teddymalaju123@users.noreply.github.com>
2023-11-15 11:44:19 +0700

    add

--- lib/screens/UserReview.dart
+++ lib/screens/UserReview.dart
@@ -34,6 +34,7 @@ class _UserReviewtState extends State<UserReview> {
   static FlutterSecureStorage storageToken = new FlutterSecureStorage();
   List<maidWork> resident = [];
   List<BookWork> bookwork = [];
+  double rating = 3.0;
 
   @override
   void initState() {
@@ -101,157 +102,181 @@ class _UserReviewtState extends State<UserReview> {
     }
   }
 
+  Future<void> updateRating(BuildContext context) async {
+    try {
+      final response = await dio.post(
+        url_api + '/books/update-Review',
+        data: {
+          'booking_id': widget.idBooking,
+          'maid_rating': rating,
+        },
+      );
+
+      if (response.statusCode == 201) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => BottomNavBar()),
+        );
+        print('success');
+      } else {
+        print('Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10.0),
-            border: Border.all(
-              color: Color.fromARGB(255, 216, 216, 216),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                color: Color.fromARGB(255, 216, 216, 216),
               ),
-            ],
-          ),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 40.0, bottom: 20),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: bookwork.isNotEmpty &&
-                                bookwork[0].profile != null
-                            ? MemoryImage(base64Decode(bookwork[0].profile!))
-                            : null,
-                      ),
-                      title: Text(
-                        "${bookwork.isNotEmpty ? bookwork[0].fname : ""} ${bookwork.isNotEmpty ? bookwork[0].lname : ""}",
-                        style: GoogleFonts.kanit(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40.0, bottom: 20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: bookwork.isNotEmpty &&
+                                  bookwork[0].profile != null
+                              ? MemoryImage(base64Decode(bookwork[0].profile!))
+                              : null,
+                        ),
+                        title: Text(
+                          "${bookwork.isNotEmpty ? bookwork[0].fname : ""} ${bookwork.isNotEmpty ? bookwork[0].lname : ""}",
+                          style: GoogleFonts.kanit(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20,
+                          ),
+                        ),
+                        subtitle: Text(
+                          "หมายเลขการจอง  ${widget.idBooking}",
+                          style: GoogleFonts.kanit(
+                            fontSize: 16,
+                          ),
                         ),
                       ),
-                      subtitle: Text("หมายเลขการจอง  ${widget.idBooking}",
-                      style: GoogleFonts.kanit(
-                          fontSize: 16,
-                        ),),
-                    ),
-                    SizedBox(height: 50),
-                    Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'รีวิวการบริการ',
-                            style: GoogleFonts.kanit(
-                              textStyle: TextStyle(color: Colors.black),
-                              fontWeight: FontWeight.w500,
-                              fontSize: 25,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          RatingBar.builder(
-                            initialRating: 3,
-                            minRating: 1,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            itemCount: 5,
-                            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                            itemBuilder: (context, _) => Icon(
-                              Icons.star,
-                              color: Colors.green,
-                            ),
-                            onRatingUpdate: (rating) {
-                              print(rating);
-                            },
-                          ),
-                          SizedBox(height: 20),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Color.fromARGB(255, 4, 148, 9),
-                              elevation: 8,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8.0),
+                      SizedBox(height: 50),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'รีวิวการบริการ',
+                              style: GoogleFonts.kanit(
+                                textStyle: TextStyle(color: Colors.black),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 25,
                               ),
                             ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => BottomNavBar()),
-                              );
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10.0,
-                                horizontal: 30.0,
+                            SizedBox(height: 20),
+                            RatingBar.builder(
+                              initialRating: 3,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemPadding:
+                                  EdgeInsets.symmetric(horizontal: 4.0),
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: Colors.green,
                               ),
-                              child: Text(
-                                'เสร็จสิ้น',
-                                style: GoogleFonts.kanit(
-                                  textStyle: TextStyle(color: Colors.white),
-                                  fontSize: 16,
+                              onRatingUpdate: (newRating) {
+                                setState(() {
+                                  rating = newRating;
+                                });
+                                print(rating);
+                              },
+                            ),
+                            SizedBox(height: 20),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Color.fromARGB(255, 4, 148, 9),
+                                elevation: 8,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
                                 ),
                               ),
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'หากมีปัญหาจากการบริการ',
-                                style: GoogleFonts.kanit(
-                                  textStyle: TextStyle(color: Colors.black54),
-                                  fontSize: 18,
+                              onPressed: () {
+                                updateRating(context);
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 10.0,
+                                  horizontal: 30.0,
                                 ),
-                              ),
-                              SizedBox(width: 8),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => ReportPage(
-                                          bookingId:widget.idBooking,
-                                        )),
-                                  );
-                                },
                                 child: Text(
-                                  'แจ้งปัญหา',
+                                  'เสร็จสิ้น',
                                   style: GoogleFonts.kanit(
-                                    decoration: TextDecoration.underline,
-                                    textStyle: TextStyle(color: Colors.red),
-                                    fontSize: 18,
+                                    textStyle: TextStyle(color: Colors.white),
+                                    fontSize: 16,
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
-                          SizedBox(height: 50),
-                        ],
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'หากมีปัญหาจากการบริการ',
+                                  style: GoogleFonts.kanit(
+                                    textStyle: TextStyle(color: Colors.black54),
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                SizedBox(width: 8),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ReportPage(
+                                                bookingId: widget.idBooking,
+                                              )),
+                                    );
+                                  },
+                                  child: Text(
+                                    'แจ้งปัญหา',
+                                    style: GoogleFonts.kanit(
+                                      decoration: TextDecoration.underline,
+                                      textStyle: TextStyle(color: Colors.red),
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 50),
+                          ],
+                        ),
                       ),
-                    ),
-                  ]),
+                    ]),
+              ),
             ),
           ),
-        ),
-      ]),
+        ]),
       ),
     );
   }
