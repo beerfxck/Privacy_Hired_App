395c8d49b9a548563d1e0c8895080435bb74ad79
Teddymalaju123 <127287397+Teddymalaju123@users.noreply.github.com>
2023-11-25 22:37:21 +0700

    add

--- lib/screens/Login_page.dart
+++ lib/screens/Login_page.dart
@@ -24,7 +24,8 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController passwordController = TextEditingController();
   bool isLoginEnabled = false;
   bool passToggle = true;
-  bool isChecked = false;
+  bool isChecked =
+      false;
 
   void checkLoginEnable() {
     setState(() {
@@ -45,43 +46,20 @@ class _LoginPageState extends State<LoginPage> {
             'password': passwordController.text,
           },
         );
+        userType = response.data["type_name"];
+        print(response);
+        await storageToken.write(
+          key: 'id_user',
+          value: response.data["id_user"].toString()
+        );
 
-        if (response.statusCode == 200) {
-          // Successful login
-          userType = response.data["type_name"];
-          await storageToken.write(
-            key: 'id_user',
-            value: response.data["id_user"].toString(),
-          );
-
-          if (userType == 'resident') {
-            GotoHome();
-          } else if (userType == 'maid') {
-            GotoMaidHome();
-          } else {
-            // Unknown user type
-            print('Unknown user type: $userType');
-          }
+        if (userType == 'resident') {
+          GotoHome();
+        } else if (userType == 'maid') {
+          GotoMaidHome();
         } else {
-          // Unsuccessful login, show alert
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text('ไม่สามารถเข้าสู่ระบบได้'),
-                content:
-                    Text('รหัสผ่านหรือชื่อผู้ใช้ไม่ถูกต้อง'),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text('ปิด'),
-                  ),
-                ],
-              );
-            },
-          );
+          // ตรวจสอบประเภทผู้ใช้ที่ไม่รู้จัก
+          print('Unknown user type: $userType');
         }
       } else {
         print('Username or password is empty.');
@@ -115,7 +93,7 @@ class _LoginPageState extends State<LoginPage> {
     Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
       MaterialPageRoute(
         builder: (BuildContext context) {
-          return MaidBottomNavBar();
+          return MaidBottomNavBar(); 
         },
       ),
       (_) => false,
@@ -126,118 +104,103 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Stack(children: [
-        // Positioned(
-        //   top: 0,
-        //   left: 0,
-        //   right: 0,
-        //   child: ClipPath(
-        //     clipper: TopCurveClipper(),
-        //     child: Container(
-        //       height: 300,
-        //       color: Color.fromARGB(255, 130, 228, 135),
-        //     ),
-        //   ),
-        // ),
-        SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: [
-                Container(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0),
-                  child: ClipRRect(
-                    child: Image(image: AssetImage('images/logo_maid.png')),
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                margin:
+                    EdgeInsets.symmetric(vertical: 100.0, horizontal: 100.0),
+                child: ClipRRect(
+                  child: Image(image: AssetImage('images/logo_maid.png')),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  controller: usernameController,
+                  onChanged: (value) {
+                    checkLoginEnable();
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text(
+                      'ชื่อผู้ใช้',
+                      style: GoogleFonts.kanit(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    prefixIcon: Icon(Icons.person_rounded),
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 25.0),
-                  child: TextField(
-                    controller: usernameController,
-                    onChanged: (value) {
-                      checkLoginEnable();
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      label: Text(
-                        'ชื่อผู้ใช้',
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: passToggle ? true : false,
+                  onChanged: (value) {
+                    checkLoginEnable();
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text('รหัสผ่าน',
                         style: GoogleFonts.kanit(
                           fontSize: 18,
                           color: Colors.grey,
+                        )),
+                    prefixIcon: Icon(Icons.lock_rounded),
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        setState(() {
+                          passToggle = !passToggle;
+                        });
+                      },
+                      child: passToggle
+                          ? Icon(CupertinoIcons.eye_slash_fill)
+                          : Icon(CupertinoIcons.eye_fill),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: SizedBox(
+                      width: 250,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
                         ),
-                      ),
-                      prefixIcon: Icon(Icons.person_rounded),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
-                  child: TextField(
-                    controller: passwordController,
-                    obscureText: passToggle ? true : false,
-                    onChanged: (value) {
-                      checkLoginEnable();
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      label: Text('รหัสผ่าน',
-                          style: GoogleFonts.kanit(
-                            fontSize: 18,
-                            color: Colors.grey,
-                          )),
-                      prefixIcon: Icon(Icons.lock_rounded),
-                      suffixIcon: InkWell(
-                        onTap: () {
-                          setState(() {
-                            passToggle = !passToggle;
-                          });
-                        },
-                        child: passToggle
-                            ? Icon(CupertinoIcons.eye_slash_fill)
-                            : Icon(CupertinoIcons.eye_fill),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: SizedBox(
-                        width: 250,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 2,
-                                blurRadius: 10,
-                                offset: Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          child: Material(
-                            color: isLoginEnabled
-                                ? Color.fromARGB(255, 11, 143, 11)
-                                : Colors.grey,
-                            borderRadius: BorderRadius.circular(10),
-                            child: InkWell(
-                              onTap: () {
-                                Login();
-                              },
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 15, horizontal: 40),
-                                child: Center(
-                                  child: Text(
-                                    "เข้าสู่ระบบ",
-                                    style: GoogleFonts.kanit(
-                                      textStyle: TextStyle(color: Colors.white),
-                                      fontSize: 18,
-                                    ),
+                        child: Material(
+                          color: isLoginEnabled
+                              ? Color.fromARGB(255, 11, 143, 11)
+                              : Colors.grey,
+                          borderRadius: BorderRadius.circular(10),
+                          child: InkWell(
+                            onTap: () {
+                              Login();
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 15, horizontal: 40),
+                              child: Center(
+                                child: Text(
+                                  "เข้าสู่ระบบ",
+                                  style: GoogleFonts.kanit(
+                                    textStyle: TextStyle(color: Colors.white),
+                                    fontSize: 18,
                                   ),
                                 ),
                               ),
@@ -246,43 +209,25 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '** หากยังไม่มีชื่อผู้ใช้และรหัสผ่านโปรดติดต่อขอรับที่นิติบุคคล **',
-                      style: GoogleFonts.kanit(
-                        textStyle: TextStyle(color: Colors.blueGrey),
-                      ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '** หากยังไม่มีชื่อผู้ใช้และรหัสผ่านโปรดติดต่อขอรับที่นิติบุคคล **',
+                    style: GoogleFonts.kanit(
+                      textStyle: TextStyle(color: Colors.blueGrey),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-      ]),
+      ),
     );
-  }
-}
-
-class TopCurveClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 70);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
